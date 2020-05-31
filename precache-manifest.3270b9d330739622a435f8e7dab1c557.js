f386e414b57727405cd84df33f303aabbff9e048
Martnick1 <matthewmartnick@gmail.com>
2020-05-31 02:41:05 -0400

    Init

---
+++ precache-manifest.3270b9d330739622a435f8e7dab1c557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cebc9cf2b441df5be3007cd2e598cf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "24918c646c66d0830cd8",
+    "url": "/portfolio/static/css/main.4d7f442e.chunk.css"
+  },
+  {
+    "revision": "b99f1bb44bc6f5adf6e1",
+    "url": "/portfolio/static/js/2.fd605abd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fd605abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24918c646c66d0830cd8",
+    "url": "/portfolio/static/js/main.9499c90b.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
