a239bf13b5e16d6a32c56b2d64c8a52973e349bf
Martnick1 <matthewmartnick@gmail.com>
2020-06-04 14:09:09 -0400

    Design page complete

--- src/components/js/Design/Logos.js
+++ src/components/js/Design/Logos.js
@@ -8,7 +8,11 @@ function Logos(props) {
         <div className="highlight">
             <section >
                 <p className="subtitle">Logo Design</p>
-                <p className="desc">Description text goes here.</p>
+                <p className="desc">                            
+                    During my Associate degree program, I had majored in Digital/Multimedia and Information Resources Design where I had studied in practice various facets of design with the Adobe Suite. 
+                    While it was highly focused on user experience/user interface design, the program also covered graphic design for print along with audio/video editing.  
+                    While in the program I found that I enjoyed logo design and actively sought out freelance work designing logos for small businesses and individuals.
+                    </p>
             </section>
 
             <div className="logos">
--- src/components/js/Design/Music.js
+++ src/components/js/Design/Music.js
@@ -8,7 +8,11 @@ function Music(props) {
         <div className="highlight">
             <section >
                 <p className="subtitle">Album Covers</p>
-                <p className="desc">Description text goes here.</p>
+                <p className="desc">
+                    Album art has always been something I have enjoyed doing and can cross the borders of photography, graphic design, typography, and illustration.
+                    Similarly creating album art requires the same skillset as other forms of design while giving space for more creative freedom.
+                    It is rare that album art won't require some sort of digital image manipulation and my experience on projects of this sort has helped to further my design skills and expertise with the Adobe Suite since before taking up a formal education.
+                    </p>
             </section>
 
             <div className="albums">
